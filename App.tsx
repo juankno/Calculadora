1140c5fbd911d38fb547487f60bd44ad7889088d
Juan Esteban <juanes0202@gmail.com>
2022-04-01 15:20:25 -0500

    First commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Calculadora</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
+
+export default App;
